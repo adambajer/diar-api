815085b2b138278f6a06a696d178fe62d901196c
adambajer <adam.bajer@gmail.com>
2024-11-16 02:01:25 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ const serviceAccount = require('./voice-noter-firebase-adminsdk-93dab-61fd824a2c
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    client_emai: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
   databaseURL: 'https://voice-noter-default-rtdb.europe-west1.firebasedatabase.app',
